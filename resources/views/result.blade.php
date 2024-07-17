ee18ebd299ff90acc9c997ab674689137452886f
Rue-ops6 <rowanmahrous01@gmail.com>
2024-07-17 22:45:50 +0300

    session4

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Submitted Information</title>
+    <title>Resulted Info.</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"

f497aa2056bae6aa6ca60bef24e02321f29540e1
Rue-ops6 <rowanmahrous01@gmail.com>
2024-07-18 06:13:29 +0300

    clazzs

--- app/Models/Clazz.php
+++ app/Models/Clazz.php
@@ -1,5 +1,5 @@
 <?php
-
+#class is reserved by PHP. Thus, we use Clazz instead.. As we used it in model as a class, it's a necessit to name the migrations as a plural and smal of Clazz = clazzs
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
--- database/migrations/2024_07_18_010614_create_clazzs_table.php
+++ database/migrations/2024_07_18_010614_create_clazzs_table.php
@@ -1,5 +1,5 @@
 <?php
-
+#class is reserved by PHP. Thus, we use Clazz instead.. As we used it in model as a class, it's a necessit to name the migrations as a plural and smal of Clazz = clazzs
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;

e1a47c260ed03b9129af38521ae598ff3ff158c5
Rue-ops6 <rowanmahrous01@gmail.com>
2024-08-07 18:45:04 +0200

    @extends('layout.main')

--- resources/views/fashiondex.blade.php
+++ resources/views/fashiondex.blade.php
@@ -1,18 +1,7 @@
 
-<!doctype html>
-<html lang="en">
+@extends('layout.main')
 
-@include('includes.fashion-head')
-
-    
-    <body>
-
-    @include('includes.fashion-preloader')
-
-    
-        <main>
-
-        @include('includes.fashion-NAV')
+@section('content')
 
 
             <section class="slick-slideshow">   
@@ -277,59 +266,8 @@
 
         </main>
 
-        <footer class="site-footer">
-            <div class="container">
-                <div class="row">
 
-                    <div class="col-lg-3 col-10 me-auto mb-4">
-                        <h4 class="text-white mb-3"><a href="index.html">Little</a> Fashion</h4>
-                        <p class="copyright-text text-muted mt-lg-5 mb-4 mb-lg-0">Copyright © 2022 <strong>Little Fashion</strong></p>
-                        <br>
-                        <p class="copyright-text">Designed by <a href="https://www.tooplate.com/" target="_blank">Tooplate</a></p>
-                    </div>
+@endsection
 
-                    <div class="col-lg-5 col-8">
-                        <h5 class="text-white mb-3">Sitemap</h5>
 
-                        <ul class="footer-menu d-flex flex-wrap">
-                            <li class="footer-menu-item"><a href="about.html" class="footer-menu-link">Story</a></li>
 
-                            <li class="footer-menu-item"><a href="#" class="footer-menu-link">Products</a></li>
-
-                            <li class="footer-menu-item"><a href="#" class="footer-menu-link">Privacy policy</a></li>
-
-                            <li class="footer-menu-item"><a href="#" class="footer-menu-link">FAQs</a></li>
-
-                            <li class="footer-menu-item"><a href="#" class="footer-menu-link">Contact</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-lg-3 col-4">
-                        <h5 class="text-white mb-3">Social</h5>
-
-                        <ul class="social-icon">
-
-                            <li><a href="#" class="social-icon-link bi-youtube"></a></li>
-
-                            <li><a href="#" class="social-icon-link bi-whatsapp"></a></li>
-
-                            <li><a href="#" class="social-icon-link bi-instagram"></a></li>
-
-                            <li><a href="#" class="social-icon-link bi-skype"></a></li>
-                        </ul>
-                    </div>
-
-                </div>
-            </div>
-        </footer>
-
-        <!-- JAVASCRIPT FILES -->
-        <script src="{{asset('assets/js/jquery.min.js')}}"></script>
-        <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-        <script src="{{asset('assets/js/Headroom.js')}}"></script>
-        <script src="{{asset('assets/js/jQuery.headroom.js')}}"></script>
-        <script src="{{asset('assets/js/slick.min.js')}}"></script>
-        <script src="{{asset('assets/js/custom.js')}}"></script>
-
-    </body>
-</html>
---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,25 @@
+
+<!doctype html>
+<html lang="en">
+
+@include('includes.fashion-head')
+
+    
+    <body>
+
+    @include('includes.fashion-preloader')
+
+    
+        <main>
+
+        @include('includes.fashion-NAV')
+
+
+
+        @yield('content')
+
+
+        @include('includes.fashion-footer')
+
+
+        @include('includes.fashion-JSfooter')
